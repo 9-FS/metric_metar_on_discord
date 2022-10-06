4350532572bd325a86fb14ac37ee24e51679ee3b
9FS <pray4spam@googlemail.com>
2022-10-06 21:03:06 +0000

    fixed TX and TN format not being changed

--- change_Format_/TXTN.py
+++ change_Format_/TXTN.py
@@ -10,10 +10,12 @@ def change_format_TXTN(info, date):
             DT+=datetime.timedelta(days=1)                                          #Tag hinzufügen
         DT+=datetime.timedelta(hours=int(info[7:9]))                                #zu Evenntdatum Uhrzeit hinzufügen
 
+        info_new=f"T{info[1]}{DT.strftime('%dT%H')}/{info[2:4]}°C"
+
         if ("temp_min" in WEATHER_MIN and int(info[2:4])<WEATHER_MIN["temp_min"]) or ("temp_max" in WEATHER_MIN and WEATHER_MIN["temp_max"]<int(info[2:4])):    #wenn Temperatur außerhalb Grenzen, wegen Leistungstabellen und Vereisung
             info_new=f"**{info_new}**"
 
-        return " "+info
+        return " "+info_new
 
     if re.search("^T[XN]M[0-9][0-9]/[0-3][0-9][0-2][0-9]Z$", info)!=None:
         DT=datetime.datetime(int(date[0:4]), int(date[5:7]), int(date[8:10]), 0, 0) #Eventdatum, initialisiert mit Veröffentlichungsdatum
@@ -21,7 +23,9 @@ def change_format_TXTN(info, date):
             DT+=datetime.timedelta(days=1)                                          #Tag hinzufügen
         DT+=datetime.timedelta(hours=int(info[8:10]))                               #zu Evenntdatum Uhrzeit hinzufügen
 
+        info_new=f"T{info[1]}{DT.strftime('%dT%H')}/{info[2:4]}°C"
+
         if ("temp_min" in WEATHER_MIN and int(info[3:5])<WEATHER_MIN["temp_min"]) or ("temp_max" in WEATHER_MIN and WEATHER_MIN["temp_max"]<int(info[3:5])):    #wenn Temperatur außerhalb Grenzen, wegen Leistungstabellen und Vereisung
             info_new=f"**{info_new}**"
         
-        return " "+info
+        return " "+info_new
