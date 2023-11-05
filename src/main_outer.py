c4b2e9c9eec773b7f634cab356220ae8e2b040a2
9FS <pray4spam@googlemail.com>
2023-11-05 19:34:59 +0000

    added "if __name__=="__main__":" guard

--- src/main_outer.py
+++ src/main_outer.py
@@ -1,17 +1,18 @@
 # Copyright (c) 2023 구FS, all rights reserved. Subject to the CC BY-NC-SA 4.0 licence in `licence.md`.
-import asyncio
-from KFSlog import KFSlog
-import logging
-import traceback
-from main import main
+if __name__=="__main__":
+    import asyncio
+    from KFSlog import KFSlog
+    import logging
+    import traceback
+    from main import main
 
 
-KFSlog.setup_logging("", logging.INFO)
-#KFSlog.setup_logging("", logging.DEBUG, filepath_format="./log/%Y-%m-%dT%H_%M.log", rotate_filepath_when="M")
+    KFSlog.setup_logging("", logging.INFO)
+    #KFSlog.setup_logging("", logging.DEBUG, filepath_format="./log/%Y-%m-%dT%H_%M.log", rotate_filepath_when="M")
 
-try:
-    asyncio.run(main())
-except:
-    logging.critical(traceback.format_exc())
-    print("\nPress enter to close program.", flush=True)
-    input() # pause
+    try:
+        asyncio.run(main())
+    except:
+        logging.critical(traceback.format_exc())
+        print("\nPress enter to close program.", flush=True)
+        input() # pause
